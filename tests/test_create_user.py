7058cd371481c76c885969888730f072f7ea23fa
Denys Filichkin <denys.philichkin@gmail.com>
2024-06-14 19:00:13 +0300

    rm email

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -3,9 +3,9 @@ from .test_main import client, setup_database
 def test_create_user(setup_database):
     response = client.post(
         "/users/",
-        json={"username": "testuser", "email": "test@example.com", "password": "password", "telegram_id": 123456, "first_name": "Test", "language_code": "en"}
+        json={"username": "testuser", "password": "password", "telegram_id": 123456, "first_name": "Test", "language_code": "en"}
     )
     assert response.status_code == 200
     data = response.json()
     assert data["username"] == "testuser"
-    assert data["email"] == "test@example.com"
+    
--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -3,7 +3,7 @@ from .test_main import client, setup_database
 def test_login_user(setup_database):
     response = client.post(
         "/login/",
-        json={"email": "test@example.com", "password": "password"}
+        json={"password": "password"}
     )
     assert response.status_code == 200
     data = response.json()
@@ -13,7 +13,7 @@ def test_login_user(setup_database):
 def test_invalid_login_user(setup_database):
     response = client.post(
         "/login/",
-        json={"email": "test@example.com", "password": "wrong password"}
+        json={"password": "wrong password"}
     )
     assert response.status_code == 400
     data = response.json()
