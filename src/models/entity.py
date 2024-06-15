ede601439a50571c235a9ee31a6ab1ce65033337
Denys Filichkin <denys.philichkin@gmail.com>
2024-06-15 19:39:18 +0300

    save

--- src/models/entity.py
+++ src/models/entity.py
@@ -2,7 +2,7 @@ import enum
 from datetime import datetime
 from typing import List
 from sqlalchemy import String, Enum, Boolean, Integer, DateTime, ForeignKey, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, backref
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -12,34 +12,40 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    chat_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), unique=True)
     tg_id: Mapped[int] = mapped_column(Integer, nullable=False, unique=True)
+    
     username: Mapped[str] = mapped_column(String(50), nullable=False)
     lastname: Mapped[str] = mapped_column(String(50), nullable=False)
-    chat_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), unique=True)
-    tasks: Mapped[List["Task"]] = relationship("Task", back_populates="user")
     points: Mapped[int] = mapped_column(Integer, default=0)
     language_code: Mapped[str] = mapped_column(String(10))
+
+    tasks: Mapped[List["Task"]] = relationship("Task", back_populates="user")
     referrals: Mapped[List["Referral"]] = relationship("Referral", back_populates="referrer")
     
-    # def update_username(self, new_username: str):
-    #     if self.username != new_username:
-    #         self.username = new_username
     @staticmethod
     def get_user_by_chat_id(session, chat_id):
         return session.query(User).filter_by(chat_id=chat_id).first()
 
 class Task(Base):
     __tablename__ = "tasks"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str] = mapped_column(String(50), nullable=False)
     completed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False) 
-    user: Mapped[User] = relationship("User", back_populates="tasks")
+    user: Mapped["User"] = relationship("User", back_populates="tasks")
 
 class Referral(Base):
     __tablename__ = "referrals"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
     referrer_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
     referred_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+
     referrer: Mapped["User"] = relationship("User", foreign_keys=[referrer_id], back_populates="referrals")
     referred: Mapped["User"] = relationship("User", foreign_keys=[referred_id])
+
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -6,19 +6,21 @@ from src.models.entity import User, Base
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     with DBSession() as session:
-        with session.begin():
-            try: # Check if user doesnt exist create user
-                chat_id = update.message.chat_id
-                user = User.get_user_by_chat_id(session, chat_id)
-                if not user:
+        try: # Check if user doesnt exist create user
+             with session.begin():
+                 chat_id = update.message.chat_id
+                 user = User.get_user_by_chat_id(session, chat_id)
+                 if not user:
                     user = update.effective_user
                     new_user = User(name=user.name, lastname=user.last_name, username=user.username,
                                     chat_id=user.id)
                     session.add(new_user)
-            except:
-                session.rollback()
-            finally:
-                session.commit()
-                context.user_data['user'] = user
-                await update.message.reply_text(f"Dear {user.name} you are registered successfully.")
+                    session.commit()
+                    context.user_data['user'] = user
+                    await update.message.reply_text(f"Dear {user.name} you are registered successfully.")
+        except Exception as er:
+            session.rollback()
+            await update.message.reply_text(f"An error occurred: {er}")
+
+            
             
