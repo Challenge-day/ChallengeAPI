96614fd1a4dec3a2d1a466917055f8c0407c4c47
Denys Filichkin <denys.philichkin@gmail.com>
2024-06-26 18:40:32 +0300

    add routes

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,16 +1,35 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from src.schemas.users import UserCreate
 from src.models.entity import User
 from src.db.connect import get_db
 
 router = APIRouter()
 
 
-@router.post("/users/{tg_id}")
+@router.get("/users/{tg_id}")
 async def get_user_by_chat_id(tg_id: int, db: Session = Depends(get_db)):
     
     user = User.get_user_by_chat_id(db, tg_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+@router.post("/users")
+async def create_user(user_info: UserCreate, db: Session = Depends(get_db)):
+    existing_user = User.get_user_by_chat_id(db, user_info.tg_id)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+    
+    new_user = User(
+        first_name=user_info.first_name,
+        username=user_info.username,
+        tg_id=user_info.tg_id,
+        language_code=user_info.language_code
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
